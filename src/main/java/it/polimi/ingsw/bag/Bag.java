0b359539e3dddaf31e658ada536d544abcdaf067
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-03-13 12:34:37 +0100

    Using Student class for Bag and Cloud

--- src/main/java/it/polimi/ingsw/bag/Bag.java
+++ src/main/java/it/polimi/ingsw/bag/Bag.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.bag;
 
+import it.polimi.ingsw.pawn.Student;
+
 import java.util.List;
 
 class Bag {
-    private List<Integer> students;
+    private List<Student> students;
 
     public Bag(int studentsPerColorCount) {
         // TODO: Create the array of students
--- src/main/java/it/polimi/ingsw/cloud/Cloud.java
+++ src/main/java/it/polimi/ingsw/cloud/Cloud.java
@@ -1,24 +1,23 @@
 package it.polimi.ingsw.cloud;
 
-class FakeStudent {}
+import it.polimi.ingsw.pawn.Student;
 
 // Package-only accessible class (only the Factory can instantiate it)
 class Cloud {
-    // TODO: Use Student instead of FakeStudent
-    private FakeStudent[] students;
+    private Student[] students;
 
     public Cloud(int studentsCount) {
-        students = new FakeStudent[studentsCount];
+        students = new Student[studentsCount];
     }
 
-    public FakeStudent[] getStudents() {
+    public Student[] getStudents() {
         // remove the students from the cloud and return them
-        FakeStudent[] returnValue = students;
+        Student[] returnValue = students;
         students = null;
         return returnValue;
     }
 
-    public void putStudents(FakeStudent[] newStudents) {
+    public void putStudents(Student[] newStudents) {
         students = newStudents;
     }
 }
