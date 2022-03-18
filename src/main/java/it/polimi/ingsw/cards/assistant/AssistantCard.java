965fbe273c0745f2b817b79b1995c6628699ac9a
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-03-18 17:57:57 +0100

    Smarter way to index AssistantCards
    Use Java internal Enum.ordinal and Enum.values()[ordinal] to serialize and deserialize AssistantCard enum

--- src/main/java/it/polimi/ingsw/cards/assistant/AssistantCard.java
+++ src/main/java/it/polimi/ingsw/cards/assistant/AssistantCard.java
@@ -1,10 +1,15 @@
 package it.polimi.ingsw.cards.assistant;
 
-public class AssistantCard {
+public enum AssistantCard {
+    // TODO: Add all the cards
+    CARD_1(3, 5),
+    CARD_2(5, 2);
+
+
     private final int turnOrderValue;
     private final int maxMotherNatureMovementValue;
 
-    public AssistantCard(int turnOrderValue, int maxMotherNatureMovementValue) {
+    AssistantCard(int turnOrderValue, int maxMotherNatureMovementValue) {
         this.turnOrderValue = turnOrderValue;
         this.maxMotherNatureMovementValue = maxMotherNatureMovementValue;
     }
@@ -15,5 +20,13 @@ public class AssistantCard {
 
     public int getMaxMotherNatureMovementValue() {
         return maxMotherNatureMovementValue;
+    }
+
+    public static AssistantCard getCardByIndex(int ordinalIndex) {
+        return AssistantCard.values()[ordinalIndex];
+    }
+
+    public static int getIndexByCard(AssistantCard card) {
+        return card.ordinal();
     }
 }
