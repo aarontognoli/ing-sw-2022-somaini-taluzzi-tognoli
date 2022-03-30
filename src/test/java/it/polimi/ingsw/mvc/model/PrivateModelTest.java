2449dd2f00ecf703f966856605b75ee79adaab9b
aarontognoli <aaron.tognoli@mail.polimi.it>
2022-03-30 16:56:13 +0200

    Tested addStudentToIsland()

--- src/test/java/it/polimi/ingsw/mvc/model/PrivateModelTest.java
+++ src/test/java/it/polimi/ingsw/mvc/model/PrivateModelTest.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.mvc.model;
 import it.polimi.ingsw.enums.Color;
 import it.polimi.ingsw.exceptions.NotFoundException;
 import it.polimi.ingsw.pawn.Student;
+import it.polimi.ingsw.places.Island;
 import it.polimi.ingsw.player.Board;
 import org.junit.jupiter.api.Test;
 
@@ -12,50 +13,53 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrivateModelTest {
+    Model model = new Model();
 
     @Test
     void getProfessorOwner() {
 
     }
+
     @Test
     void removeStudentFromEntrance() {
-        Model model = new Model();
+
 
         Board testBoard = new Board();
         List<Student> tempEntrance = new ArrayList<>();
-        for(int i = 0; i<7;i++)
-        {
-            tempEntrance.add(new Student(Color.YELLOW_GNOMES,i));
+        for (int i = 0; i < 7; i++) {
+            tempEntrance.add(new Student(Color.YELLOW_GNOMES, i));
         }
         try {
             model.privateModel.removeStudentFromEntrance(new Student(Color.GREEN_FROGS, 7), testBoard);
             assert false;
-        }
-        catch (NotFoundException e)
-        {
+        } catch (NotFoundException e) {
             assert true;
         }
         testBoard.addStudentsToEntrance(tempEntrance);
         try {
             model.privateModel.removeStudentFromEntrance(new Student(Color.GREEN_FROGS, 7), testBoard);
             assert false;
-        }
-        catch (NotFoundException e)
-        {
+        } catch (NotFoundException e) {
             assert true;
         }
         try {
             Student s = model.privateModel.removeStudentFromEntrance(new Student(Color.YELLOW_GNOMES, 7), testBoard);
-            assertEquals(s.getColor(),Color.YELLOW_GNOMES);
-        }
-        catch (NotFoundException e)
-        {
+            assertEquals(s.getColor(), Color.YELLOW_GNOMES);
+            assertEquals(testBoard.getEntrance().size(), 6);
+        } catch (NotFoundException e) {
             assert false;
         }
     }
 
     @Test
     void addStudentToIsland() {
+        Island island = new Island();
+        model.privateModel.addStudentToIsland(new Student(Color.YELLOW_GNOMES, 0), island);
+        assertEquals(island.getStudents().size(), 1);
+        assertEquals(island.getStudents().get(0).getID(), 0);
+        assertEquals(island.getStudents().get(0).getColor(), Color.YELLOW_GNOMES);
+
+
     }
 
     @Test
