4c46ac2a6747419030ebf32ab3816878e46e1217
aarontognoli <aaron.tognoli@mail.polimi.it>
2022-04-05 14:59:15 +0200

    Implemented CentaurCharacter

---
+++ src/main/java/it/polimi/ingsw/cards/characters/CentaurCharacter/CentaurCharacter.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.cards.characters.CentaurCharacter;
+
+import it.polimi.ingsw.cards.characters.CCArgumentException;
+import it.polimi.ingsw.cards.characters.CharacterCard;
+import it.polimi.ingsw.mvc.model.InfluenceCalculatorCentaur;
+import it.polimi.ingsw.mvc.model.Model;
+
+public class CentaurCharacter extends CharacterCard {
+
+    public CentaurCharacter(Model model) {
+        super(model, 3);
+    }
+
+    @Override
+    protected void internalActivateEffect(Object arguments) throws CCArgumentException {
+        if (arguments != null) {
+            throw new CCArgumentException(CCArgumentException.INVALID_CLASS_MESSAGE);
+        }
+        model.characterModel.updateInfluenceCalculator(new InfluenceCalculatorCentaur(model));
+    }
+}
