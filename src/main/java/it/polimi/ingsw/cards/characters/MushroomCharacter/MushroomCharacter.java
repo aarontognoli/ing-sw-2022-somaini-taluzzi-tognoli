bafefa2afd9e9cd78ca4d5e58c70d74a0f0f7d40
aarontognoli <aaron.tognoli@mail.polimi.it>
2022-04-05 15:31:06 +0200

    Fixed KnightCharacter and Implemented KnightCharacter

--- src/main/java/it/polimi/ingsw/cards/characters/MushroomCharacter/MushroomCharacter.java
+++ src/main/java/it/polimi/ingsw/cards/characters/MushroomCharacter/MushroomCharacter.java
@@ -1,25 +1,25 @@
-package it.polimi.ingsw.cards.characters.KnightCharacter;
+package it.polimi.ingsw.cards.characters.MushroomCharacter;
 
 import it.polimi.ingsw.cards.characters.CCArgumentException;
 import it.polimi.ingsw.cards.characters.CharacterCard;
-import it.polimi.ingsw.mvc.model.InfluenceCalculatorCentaur;
+import it.polimi.ingsw.enums.Color;
 import it.polimi.ingsw.mvc.model.InfluenceCalculatorKnight;
+import it.polimi.ingsw.mvc.model.InfluenceCalculatorMushroom;
 import it.polimi.ingsw.mvc.model.Model;
 
-public class KnightCharacter extends CharacterCard {
-
-    public KnightCharacter(Model model) {
+public class MushroomCharacter extends CharacterCard {
+    public MushroomCharacter(Model model) {
         super(model, 2);
     }
 
     @Override
     protected void internalActivateEffect(Object arguments) throws CCArgumentException {
-        if (!(arguments instanceof Integer)) {
+        if (!(arguments instanceof Color)) {
             throw new CCArgumentException(CCArgumentException.INVALID_CLASS_MESSAGE);
         }
 
-        int targetPlayerIndex = (int) arguments;
+        Color targetColor = (Color) arguments;
 
-        model.characterModel.updateInfluenceCalculator(new InfluenceCalculatorKnight(model,targetPlayerIndex));
+        model.characterModel.updateInfluenceCalculator(new InfluenceCalculatorMushroom(model,targetColor));
     }
 }
