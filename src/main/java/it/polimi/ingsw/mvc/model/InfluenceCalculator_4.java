bcf307f1e64b32b8f5cad6c1b0a79975787c7a4d
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-04-05 14:37:18 +0200

    Implemented InfluenceCalculator for 4 players

--- src/main/java/it/polimi/ingsw/mvc/model/InfluenceCalculator_4.java
+++ src/main/java/it/polimi/ingsw/mvc/model/InfluenceCalculator_4.java
@@ -1,7 +1,13 @@
 package it.polimi.ingsw.mvc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.polimi.ingsw.enums.Color;
+import it.polimi.ingsw.pawn.Student;
 import it.polimi.ingsw.places.Island;
 import it.polimi.ingsw.player.Board;
+import it.polimi.ingsw.player.Player;
 
 public class InfluenceCalculator_4 extends InfluenceCalculator {
     public InfluenceCalculator_4(Model fatherModel) {
@@ -11,11 +17,51 @@ public class InfluenceCalculator_4 extends InfluenceCalculator {
     /**
      * @param island Island in consideration
      * @return null if tie;
-     * otherwise the board of the player of the time that has the towers (only one of the team has the towers)
+     *         otherwise the board of the player of the time that has the towers
+     *         (only one of the team has the towers)
      */
     @Override
     Board getInfluence(Island island) {
-        // TODO: Implementation upon this.influenceRules
-        return null;
+        int currentPlayerInfluence;
+
+        int teamOneInfluence = 0;
+        int teamTwoInfluence = 0;
+
+        for (int playerIndex = 0; playerIndex < fatherModel.totalPlayerCount; playerIndex++) {
+            currentPlayerInfluence = 0;
+
+            // Compute tower influence
+            // [!] CentaurCharacter denies tower influence
+            currentPlayerInfluence += influenceRules.getTowerInfluence(playerIndex, island);
+
+            boolean[] currentPlayerHasProfessor = new boolean[Color.values().length];
+            for (Color color : Color.values()) {
+                // Check if this player has the professor of this color
+                // [!] MushroomCharacter denies the influence of a color
+                currentPlayerHasProfessor[color.ordinal()] = influenceRules.hasProfessorOfColor(playerIndex, color);
+            }
+
+            for (Student s : island.getStudents()) {
+                if (currentPlayerHasProfessor[s.getColor().ordinal()]) {
+                    currentPlayerInfluence++;
+                }
+            }
+
+            currentPlayerInfluence += influenceRules.incrementInfluence(playerIndex);
+
+            if (playerIndex <= 1) {
+                teamOneInfluence += currentPlayerInfluence;
+            } else {
+                teamTwoInfluence += currentPlayerInfluence;
+            }
+        }
+
+        if (teamOneInfluence == teamTwoInfluence) {
+            return null;
+        } else if (teamOneInfluence > teamTwoInfluence) {
+            return fatherModel.players.get(0).getBoard();
+        } else {
+            return fatherModel.players.get(2).getBoard();
+        }
     }
 }
