97cb56136077815a2eaa991e29351817b3fee3da
aarontognoli <aaron.tognoli@mail.polimi.it>
2022-04-08 15:14:40 +0200

    Implemented PipeCharacter

---
+++ src/main/java/it/polimi/ingsw/cards/characters/PipeCharacter/PipeCharacter.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.cards.characters.PipeCharacter;
+
+import it.polimi.ingsw.cards.characters.CCArgumentException;
+import it.polimi.ingsw.cards.characters.CharacterCard;
+import it.polimi.ingsw.enums.Color;
+import it.polimi.ingsw.mvc.model.Model;
+import it.polimi.ingsw.player.Player;
+
+import java.nio.channels.Pipe;
+
+public class PipeCharacter extends CharacterCard {
+    public PipeCharacter(Model model)
+    {
+        super(model,3);
+    }
+
+    @Override
+    protected void internalActivateEffect(Object arguments) throws CCArgumentException {
+        if (!(arguments instanceof Color)) {
+            throw new CCArgumentException(CCArgumentException.INVALID_CLASS_MESSAGE);
+        }
+        Color whichColor = (Color) arguments;
+        try {
+            model.characterModel.removeStudentsFromAllBoards(3, whichColor);
+        }catch (IllegalArgumentException e)
+        {
+            throw new RuntimeException("This should never happen");
+        }
+
+    }
+}
--- src/main/java/it/polimi/ingsw/mvc/model/CharacterModel.java
+++ src/main/java/it/polimi/ingsw/mvc/model/CharacterModel.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.mvc.model;
 
 import it.polimi.ingsw.bag.BagEmptyException;
+import it.polimi.ingsw.enums.Color;
 import it.polimi.ingsw.pawn.Student;
+import it.polimi.ingsw.player.Player;
+
+import java.util.List;
 
 public class CharacterModel {
     final Model fatherModel;
@@ -17,8 +21,19 @@ public class CharacterModel {
     public void updateProfessorMoverRule(ProfessorMoverRuleDefault newProfessorMoverRule) {
         fatherModel.professorMoverRule = newProfessorMoverRule;
     }
+
     public void updateInfluenceCalculator(InfluenceCalculatorRules newInfluenceCalculatorRules) {
         fatherModel.influenceCalculator.setInfluenceCalculatorRules(newInfluenceCalculatorRules);
     }
 
+    public void removeStudentsFromAllBoards(int howMany, Color whichColor) throws IllegalArgumentException{
+        for (Player p : fatherModel.players) {
+            List<Student> diningRoom = p.getBoard().getDiningRoom().get(whichColor.ordinal());
+            for (int i = 0; i < howMany; i++) {
+                if (!diningRoom.isEmpty()) {
+                    fatherModel.bag.reinsert(diningRoom.remove(diningRoom.size() - 1));
+                }
+            }
+        }
+    }
 }
