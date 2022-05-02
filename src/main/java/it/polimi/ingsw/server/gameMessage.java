c9ba1da2fef0be9445224cfb8803eddc565e3d9b
aarontognoli <aaron.tognoli@mail.polimi.it>
2022-05-02 19:58:26 +0200

    added match actions checks

--- src/main/java/it/polimi/ingsw/server/gameMessage.java
+++ src/main/java/it/polimi/ingsw/server/gameMessage.java
@@ -11,6 +11,10 @@ public class gameMessage {
     public static String winMessage = "You win!";
     public static String loseMessage = "You lose!";
     public static String wrongTurnMessage = "It is not your turn!";
-    public static String playerAlreadyWonMessage = "Forbidden move, match already won by player ";
-    public static String wrongGamePhaseMessage = "Forbidden move, wrong Game Phase";
+    public static String playerAlreadyWonMessage = "match already won by player ";
+    public static String wrongGamePhaseMessage = "wrong Game Phase";
+    public static String characterCardAlreadyPlayedMessage = "character card already played";
+    public static String maxStudentsAlreadyPlacedMessage = "maximum number of students already placed";
+    public static String notEnoughStudentsAlreadyPlaced = "you need to place more students";
+    public static String motherNatureNotMovedMessage = "you need to move Mother Nature First";
 }
