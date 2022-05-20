087b023a5e00715ce1a89f3621d74728b2d9140c
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-05-20 18:53:25 +0200

    Fixed NetworkArchitectureTest:
    1. trying multiple time to bind serverSocket, sleeping for a time that grows exponentially
    2. Removed busy wait in asyncWriteToSocket, using Locks now
    3. Removed polling in waitToReceiveMessage, using Locks now

--- src/test/java/it/polimi/ingsw/match/NetworkArchitectureTest.java
+++ src/test/java/it/polimi/ingsw/match/NetworkArchitectureTest.java
@@ -22,7 +22,6 @@ import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,8 +35,8 @@ public class NetworkArchitectureTest {
         assertTrue(client1.isActive());
         assertTrue(client2.isActive());
         client1.sendMessage(new SetNicknameMessage("Prova"));
-      
-        assertEquals(ErrorMessage.class, client1.waitToRecieveMessage().getClass());
+
+        assertEquals(ErrorMessage.class, client1.waitToReceiveMessage().getClass());
 
         assertDoesNotThrow(server::closeServer);
 
@@ -56,88 +55,98 @@ public class NetworkArchitectureTest {
 
         //c1 creates lobby Prova
         client1.sendMessage(new CreateLobbyMessage("Prova", 2, GameMode.EXPERT_MODE, 1));
-        received1 = client1.waitToRecieveMessage();
+        received1 = client1.waitToReceiveMessage();
         assertEquals(LobbyCreationAckMessage.class, received1.getClass());
         assertTrue(((LobbyCreationAckMessage) received1).isNameValid());
         assertTrue(((LobbyCreationAckMessage) received1).areOptionsValid());
 
         //c2 tries to create same lobby
         client2.sendMessage(new CreateLobbyMessage("Prova", 0, GameMode.EXPERT_MODE, 1));
-        received2 = client2.waitToRecieveMessage();
+        received2 = client2.waitToReceiveMessage();
         assertEquals(LobbyCreationAckMessage.class, received2.getClass());
         assertFalse(((LobbyCreationAckMessage) received2).isNameValid());
         assertFalse(((LobbyCreationAckMessage) received2).areOptionsValid());
 
         client2.sendMessage(new JoinLobbyMessage("Prova"));
-        received2 = client2.waitToRecieveMessage();
+        received2 = client2.waitToReceiveMessage();
         assertEquals(LobbyNameAckMessage.class, received2.getClass());
         assertTrue(((LobbyNameAckMessage) received2).isValid());
 
 
         //c3 tries to join lobby prova, which is full
         client3.sendMessage(new RequestLobbyNamesListMessage());
-        received3 = client3.waitToRecieveMessage();
+        received3 = client3.waitToReceiveMessage();
         assertEquals(LobbyNamesListMessage.class, received3.getClass());
         assertEquals(2, ((LobbyNamesListMessage) received3).getLobbies().get(0).currentPlayersCount());
         client3.sendMessage(new JoinLobbyMessage("Prova"));
-        received3 = client3.waitToRecieveMessage();
+        received3 = client3.waitToReceiveMessage();
         assertEquals(LobbyNameAckMessage.class, received3.getClass());
         assertFalse(((LobbyNameAckMessage) received3).isValid());
         //client1 inserts username and deck
         client1.sendMessage(new SetNicknameMessage("one"));
-        received1 = client1.waitToRecieveMessage();
+        received1 = client1.waitToReceiveMessage();
         assertEquals(SetNicknameAckMessage.class, received1.getClass());
         assertFalse(((SetNicknameAckMessage) received1).isUsed());
         client1.sendMessage(new SetDeckMessage(DeckName.DESERT_KING));
-        received1 = client1.waitToRecieveMessage();
+        received1 = client1.waitToReceiveMessage();
         assertEquals(SetDeckAckMessage.class, received1.getClass());
         assertTrue(((SetDeckAckMessage) received1).isDeckValid());
 
         //client2 tries to insert same username and deck
         client2.sendMessage(new SetNicknameMessage("one"));
-        received2 = client2.waitToRecieveMessage();
+        received2 = client2.waitToReceiveMessage();
         assertEquals(SetNicknameAckMessage.class, received2.getClass());
         assertTrue(((SetNicknameAckMessage) received2).isUsed());
 
         client2.sendMessage(new SetNicknameMessage("two"));
-        received2 = client2.waitToRecieveMessage();
+        received2 = client2.waitToReceiveMessage();
         assertEquals(SetNicknameAckMessage.class, received2.getClass());
         assertFalse(((SetNicknameAckMessage) received2).isUsed());
 
         client2.sendMessage(new SetDeckMessage(DeckName.DESERT_KING));
-        received2 = client2.waitToRecieveMessage();
+        received2 = client2.waitToReceiveMessage();
         assertEquals(SetDeckAckMessage.class, received2.getClass());
         assertFalse(((SetDeckAckMessage) received2).isDeckValid());
 
         client2.sendMessage(new SetDeckMessage(DeckName.CLOUD_WITCH));
-        received2 = client2.waitToRecieveMessage();
+        received2 = client2.waitToReceiveMessage();
         assertEquals(SetDeckAckMessage.class, received2.getClass());
         assertTrue(((SetDeckAckMessage) received2).isDeckValid());
 
 
-        received2 = client2.waitToRecieveMessage();
+        received2 = client2.waitToReceiveMessage();
         assertEquals(GameStartMessage.class, received2.getClass());
 
 
-        received1 = client1.waitToRecieveMessage();
+        received1 = client1.waitToReceiveMessage();
         assertEquals(GameStartMessage.class, received1.getClass());
 
 
         System.out.println("Ended Lobby network test");
-      
+
         assertDoesNotThrow(server::closeServer);
 
     }
 
     private Server runServerAsync() {
-        Server server;
-        try {
-            server = new Server();
-        } catch (IOException e) {
-            e.printStackTrace();
-            assert false;
-            return null;
-        }
+        Server server = null;
+        int serverBindAttempts = 0;
+        do {
+            try {
+                server = new Server();
+            } catch (IOException e) {
+                serverBindAttempts++;
+
+                try {
+                    Thread.sleep(10L * serverBindAttempts * serverBindAttempts);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                    assert false;
+                }
+            }
+        } while (serverBindAttempts < 10 && server == null);
+
+        assertNotNull(server);
 
         Thread t = new Thread(server::runServer);
         t.start();
@@ -160,23 +169,20 @@ public class NetworkArchitectureTest {
         return client;
     }
 
-    private class ClientStub {
+    private static class ClientStub {
+        public final List<Message> messageList = new LinkedList<>();
         private final String ip;
         private final int port;
+        private final Object messageToSendLock = new Object();
         Socket socket;
         private Message message;
         private boolean messageToSend = false;
-        private final Object messageToSendLock = new Object();
-        public boolean recieved = false;
-        private Message recievedMessage;
-        public List<Message> messageList = new LinkedList<>();
+        private boolean active = true;
 
         public ClientStub(String ip, int port) {
             this.ip = ip;
             this.port = port;
         }
-
-        private boolean active = true;
 
         public synchronized boolean isActive() {
             return active;
@@ -193,8 +199,10 @@ public class NetworkArchitectureTest {
                         Object inputObject = socketIn.readObject();
                         if (inputObject instanceof Message message) {
                             System.out.println(message);
-                            recieved = true;
-                            messageList.add(message);
+                            synchronized (messageList) {
+                                messageList.add(message);
+                                messageList.notifyAll();
+                            }
                         } else if (inputObject instanceof Model) {
                             System.out.println("New Model received");
                         } else {
@@ -214,13 +222,12 @@ public class NetworkArchitectureTest {
                 try {
                     while (isActive()) {
                         synchronized (messageToSendLock) {
-                            if (messageToSend) {
+                            while (!messageToSend) messageToSendLock.wait();
 
-                                socketOut.writeObject(message);
-                                socketOut.flush();
-                                socketOut.reset();
-                                messageToSend = false;
-                            }
+                            socketOut.writeObject(message);
+                            socketOut.flush();
+                            socketOut.reset();
+                            messageToSend = false;
                         }
                     }
                 } catch (Exception e) {
@@ -236,40 +243,30 @@ public class NetworkArchitectureTest {
             messageToSend = true;
         }
 
-        public Message waitToRecieveMessage() {
-            do {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(250);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        public Message waitToReceiveMessage() {
+            synchronized (messageList) {
+                while (messageList.isEmpty())
+                    assertDoesNotThrow(() -> messageList.wait());
 
-            } while (messageList.isEmpty());
-
-            return messageList.remove(0);
-        }
-
-        public void setMessage(Message message) {
-            this.message = message;
-            synchronized (messageToSendLock) {
-                messageToSend = true;
+                return messageList.remove(0);
             }
         }
 
-        public void close() throws IOException {
-            if (!socket.isClosed()) {
-                socket.shutdownOutput();
-                socket.shutdownInput();
-                socket.close();
+        public void setMessage(Message message) {
+            synchronized (messageToSendLock) {
+                this.message = message;
+                messageToSend = true;
+                messageToSendLock.notifyAll();
             }
         }
 
         public void run() throws IOException {
             socket = new Socket(ip, port);
             System.out.println("Connection established");
-            ObjectOutputStream socketOut = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream socketIn = new ObjectInputStream(socket.getInputStream());
-            try {
+            try (
+                    ObjectOutputStream socketOut = new ObjectOutputStream(socket.getOutputStream());
+                    ObjectInputStream socketIn = new ObjectInputStream(socket.getInputStream())
+            ) {
                 Thread t0 = asyncReadFromSocket(socketIn);
                 Thread t1 = asyncWriteToSocket(socketOut);
                 t0.join();
@@ -277,8 +274,6 @@ public class NetworkArchitectureTest {
             } catch (InterruptedException | NoSuchElementException e) {
                 System.out.println("Connection closed from the client side");
             } finally {
-                socketIn.close();
-                socketOut.close();
                 socket.close();
             }
         }
